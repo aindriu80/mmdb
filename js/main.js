816ff7b950d99e3ee0d539ede32db4c53578311b
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-06-12 10:20:43 +0100

    Fetch & display a single movie

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
   });
 });
 
+// Before Movie details page
+$(document).on("pagebeforeshow", "#movie", function() {
+  let movieId = sessionStorage.getItem("movieId");
+  getMovie(movieId);
+});
+
 // movie clicked
 function movieClicked(id) {
   //   console.log(id);
@@ -35,6 +41,40 @@ function getMovies(searchText) {
     });
     $("#movies")
       .html(output)
+      .listview("refresh");
+  });
+}
+
+// Get single movie
+function getMovie(movieId) {
+  console.log("this", movieId);
+
+  $.ajax({
+    method: "GET",
+    url: "http://www.omdbapi.com?i=" + movieId + "&apikey=3a8b886d"
+  }).done(function(movie) {
+    console.log(movie);
+    let movieTop = `
+    <div style="text-align:center">
+    <h1>${movie.Title}</h1>
+    <img src="${movie.Poster}">
+    </div>
+    `;
+    $("#movieTop").html(movieTop);
+
+    let movieDetails = `
+    <li><strong>Genre:</strong> ${movie.Genre}</li>
+    <li><strong>Rated:</strong> ${movie.Rated}</li>
+    <li><strong>Released:</strong> ${movie.Released}</li>
+    <li><strong>Runtime:</strong> ${movie.Runtime}</li>
+    <li><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+    <li><strong>IMDB Votes:</strong> ${movie.imdbVotes}</li>
+    <li><strong>Actors:</strong> ${movie.Actors}</li>
+    <li><strong>Director:</strong> ${movie.Director}</li>
+  `;
+
+    $("#movieDetails")
+      .html(movieDetails)
       .listview("refresh");
   });
 }
