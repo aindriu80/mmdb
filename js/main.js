59b2657a81854e5fe5eaa42ff403fe1f27fe2d4e
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-06-12 09:54:20 +0100

    Get a movie ID & create a new page

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
- $(document).ready(function () {
-     $('body').on('submit', '#searchForm', function (e) {
-         let searchText = $('#searchText').val();
-         getMovies(searchText);
-         e.preventDefault();
-     })
- });
+$(document).ready(function() {
+  $("body").on("submit", "#searchForm", function(e) {
+    let searchText = $("#searchText").val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
 
- // Get Movies From OBDB API
- function getMovies(searchText) {
-     $.ajax({
-         method: 'GET',
-         url: 'http://www.omdbapi.com?s=' + searchText + '&apikey=3a8b886d'
-     }).done(function (data) {
-         console.log(data);
-         let movies = data.Search;
-         let output = '';
-         $.each(movies, function (index, movie) {
-             output += `
+// movie clicked
+function movieClicked(id) {
+  //   console.log(id);
+  sessionStorage.setItem("movieId", id);
+  $.mobile.changePage("movie.html");
+}
+
+// Get Movies From OBDB API
+function getMovies(searchText) {
+  $.ajax({
+    method: "GET",
+    url: "http://www.omdbapi.com?s=" + searchText + "&apikey=3a8b886d"
+  }).done(function(data) {
+    // console.log(data);
+    let movies = data.Search;
+    let output = "";
+    $.each(movies, function(index, movie) {
+      output += `
                      <li>
-                     <a href="#">
+                     <a onclick="movieClicked('${movie.imdbID}')" href="#">
                      <img src="${movie.Poster}">
                      <h2>${movie.Title}</h2>
                      <p>Release Year: ${movie.Year}</p>
                      </a>
                      </li>
                      `;
-         });
-         $('#movies').html(output).listview('refresh');
-     });
- }
+    });
+    $("#movies")
+      .html(output)
+      .listview("refresh");
+  });
+}
